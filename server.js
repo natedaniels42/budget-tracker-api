d63aa56d77960a157b32d3ccf96ff935b2fefe43
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-20 16:03:56 -0500

    Added routes folder and index and tranctions routes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const routes = require('./routes');
 
 const PORT = process.env.PORT;
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+app.use('/api/v1/transactions', routes.transactions);
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
