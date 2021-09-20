fdd5cd4b1fa0aa46f6e8af475c82957ea18011a7
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-20 16:40:12 -0500

    Added cors to server

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const routes = require('./routes');
 
 const PORT = process.env.PORT;
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET, POST, PUT, DELETE',
+    optionsSuccessStatus: 200
+}));
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
